1626ac73158a7da8ae560b67d9dabb3ad0f2faf0
dvlpr2003 <gayathrigaya698@gmail.com>
2025-01-27 16:55:33 +0530

    gigs

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(cors(
     {origin: "https://test-front-delta-murex.vercel.app", 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }
 ))
 
